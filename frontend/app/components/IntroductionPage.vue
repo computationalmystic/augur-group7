3ee4d92cb91b4024bd8fc343c9a4bd88527ee94a
dtsrkb <35503170+dtsrkb@users.noreply.github.com>
2019-04-19 13:49:08 -0500

    Added exports in intro

--- frontend/app/components/IntroductionPage.vue
+++ frontend/app/components/IntroductionPage.vue
@@ -30,13 +30,14 @@
       <h4>Issues For Newcomers</h4>
       </div>
   </div>
-
-  <div class="col col-6">
-    <dynamic-line-chart source="watchers" 
-       title="Watchers / Week " 
-        cite-url="https://github.com/augurlabs/wg-gmd/tree/master/activity-metrics/watchers.md"
-        cite-text="Watchers"> 
-    </dynamic-line-chart>
+  <div class="row">
+    <div class="col col-6">
+      <dynamic-line-chart source="watchers" 
+         title="Watchers / Week " 
+          cite-url="https://github.com/augurlabs/wg-gmd/tree/master/activity-metrics/watchers.md"
+          cite-text="Watchers"> 
+      </dynamic-line-chart>
+    </div>
   </div>
 </section>
 
@@ -48,32 +49,18 @@ import DynamicLineChart from './charts/DynamicLineChart'
 import BubbleChart from './charts/BubbleChart'
 import StackedBarChart from './charts/StackedBarChart'
 
-  module.exports = {
-
-    data() {
-
-      return {
-
-
-
-      }
-
-    },
-
-    methods: {
-
-
-
-    },
-
-    computed: {
-
-
-
+module.exports = {
+  data() {
+    return {
+      colors: ["#FF3647", "#4736FF","#3cb44b","#ffe119","#f58231","#911eb4","#42d4f4","#f032e6"]
     }
-
+  },
+  components: {
+    DynamicLineChart,
+    BubbleChart,
+    StackedBarChart
   }
-
+};
 
 </script>
 
