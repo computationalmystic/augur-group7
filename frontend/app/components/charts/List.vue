fafc63b1b29fd08a5b2ee2f371557b5cb0c9b0ec
Sean Goggins <s@goggins.com>
2019-04-26 15:49:21 -0500

    list.vue

--- frontend/app/components/charts/List.vue
+++ frontend/app/components/charts/List.vue
@@ -5,3 +5,33 @@
   </template>
 </ul>
 
+
+<script>
+export default {
+  props: ['language'],
+  data() {
+    return {
+      values: []
+    }
+  },
+  computed: {
+    repo() {
+      return this.$store.state.baseRepo
+    },
+    chart() {
+      $(this.$el).find('.showme').addClass('invis')
+      $(this.$el).find('.textchart').addClass('loader')
+      if (this.repo) {
+        window.AugurRepos[this.repo][this.source]().then((data) => {
+          $(this.$el).find('.showme, .hidefirst').removeClass('invis')
+          $(this.$el).find('.textchart').removeClass('loader')
+          this.values = data
+          console.log('Data:')
+          console.log(data)
+        })
+      }
+    }
+  }
+}
+
+</script>
