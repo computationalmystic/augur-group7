e5fe1bf24e9bb6067764468e86222c03833c5c8a
dtsrkb <35503170+dtsrkb@users.noreply.github.com>
2019-04-26 15:15:27 -0500

    reverting SQL

--- augur/datasources/ghtorrent/ghtorrent.py
+++ augur/datasources/ghtorrent/ghtorrent.py
@@ -167,7 +167,7 @@ class GHTorrent(object):
         """
         repoid=self.repoid(owner,repo)
         reposql = s.sql.text('SELECT projects.name FROM projects WHERE projects.id = :repoid')
-        reposql2 = s.sql.text('SELECT issues.issue_id  FROM issues, issue_events WHERE issues.repo_id = :repoid and issue_events.action != "closed" and issues.issue_id = issue_events.issue_id ORDER BY issues.created_at DESC')
+        reposql2 = s.sql.text('SELECT issues.issue_id  FROM issues WHERE issues.repo_id = :repoid ORDER BY issues.created_at DESC')
         repoJSON = pd.read_sql(reposql, self.db, params={"repoid": str(repoid)})
         repoJSON2 = pd.read_sql(reposql2, self.db, params={"repoid": str(repoid)})
         repoJSON3 = [repoJSON, repoJSON2]
