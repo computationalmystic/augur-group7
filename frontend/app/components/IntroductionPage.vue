9200a3c14885121ce5f300c5e8e5dd7f9e420d71
dtsrkb <35503170+dtsrkb@users.noreply.github.com>
2019-04-29 19:18:49 -0500

    Trying out List.vue

--- frontend/app/components/IntroductionPage.vue
+++ frontend/app/components/IntroductionPage.vue
@@ -28,6 +28,10 @@
       </div>
   <div class="row">
     <div class="col col-6">
+      <List source="getLanguages"
+        title="Project Languages"
+      </List>
+    <div class="col col-6">
       <dynamic-line-chart source="communityEngagement:issues_open"
         title="Community Engagement: Open Issues"
         cite-url="https://github.com/augurlabs/wg-gmd/blob/master/activity-metrics/open-issues.md"
