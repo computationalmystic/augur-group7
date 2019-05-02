a7547cf0e05f73a26d771465a60e53fa831d30f7
dtsrkb <35503170+dtsrkb@users.noreply.github.com>
2019-05-02 12:55:50 -0500

    ctrl-z

--- frontend/app/components/charts/IssueList.vue
+++ frontend/app/components/charts/IssueList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="item in values">{{ https://www.github.com/twitter/ + item.name + /issues/ + item.issue_id }}</li>
+      <li v-for="item in values">{{ "https://www.github.com/twitter/"+ item.name + "/issues/" + item.issue_id" }}</li>
       <li class="divider" role="presentation"></li>
     </ul>
   </div>
@@ -36,7 +36,7 @@ export default {
   },
   created(){
     let repo = window.AugurAPI.Repo({ githubURL: this.repo })
-    repo.getIssues().then((data) =>{
+    repo.getDescription().then((data) =>{
     this.values = data
     console.log('Data starts here:')
     console.log(data)
