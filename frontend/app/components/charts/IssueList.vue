742fffb4c50fde256f4e1ad7dc07cae735c13d6d
vagrant <vagrant@augur>
2019-05-02 19:38:01 +0000

    url()

--- frontend/app/components/charts/IssueList.vue
+++ frontend/app/components/charts/IssueList.vue
@@ -2,7 +2,7 @@
   <div>
     <ul>
       <li v-for="item in values">
-        <a :href="''">{{"https://www.github.com/twitter/" + item.name + "/issues/" + item.issue_id }}</a>
+        <a :href="'url(item)'">{{"https://www.github.com/twitter/" + item.name + "/issues/" + item.issue_id }}</a>
       </li>
       <li class="divider" role="presentation"></li>
     </ul>
@@ -19,6 +19,9 @@ export default {
     }
   },
   computed: {
+    url(item) {
+      return "https://www.github.com/twitter/" + item.name + "/issues/" + item.issue_id 
+    }
     repo() {
       return this.$store.state.baseRepo
     },
