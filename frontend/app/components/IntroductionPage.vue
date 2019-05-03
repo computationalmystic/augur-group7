d84fce748246d3cc0e5ebd2473bd17592149a31a
dtsrkb <35503170+dtsrkb@users.noreply.github.com>
2019-05-03 12:40:18 -0500

    script tag

--- frontend/app/components/IntroductionPage.vue
+++ frontend/app/components/IntroductionPage.vue
@@ -24,10 +24,6 @@
   <div id="row">  
       <div class="input">
         <h4>Edit Description</h4>
-
-<script src="https://unpkg.com/marked@0.3.6"></script>
-<script src="https://unpkg.com/lodash@4.16.0"></script>
-
   <div id="editor">
     <textarea :value="input" @input="update"></textarea>
   <div v-html="compiledMarkdown"></div>
@@ -94,6 +90,9 @@ module.exports = {
 };
 
 </script>
+
+<script src="https://unpkg.com/marked@0.3.6"></script>
+<script src="https://unpkg.com/lodash@4.16.0"></script>
 
 <style scoped>
 
